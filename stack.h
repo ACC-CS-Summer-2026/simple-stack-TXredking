c4f5265ca89c342b2c6bade68f636a260395e3d4
Rory McLaughlin <15098638+TXredking@users.noreply.github.com>
2026-06-21 22:09:17 -0500

    Update stack.h with Stack class methods and attributes.

--- stack.h
+++ stack.h
@@ -6,3 +6,47 @@
 *  Purpose: Header file for stack.cpp. All directives for
 *  stack go here.
 ************************************************* */
+
+#ifndef CLASSES_STACK_H
+#define CLASSES_STACK_H
+
+#define STACKSIZE 10
+
+class Stack {
+
+    public:
+    /**********************
+    Constructors/Destructor
+    ***********************/
+    Stack();
+    ~Stack();
+
+    /**********************
+    Getters/Accessors
+    ***********************/
+    int pop();
+    int peek();
+    bool isEmpty();
+
+    /**********************
+    Setters/Mutators
+    ***********************/
+    bool push(int);
+
+    /**********************
+    Printing
+    ***********************/
+
+private:
+    /**********************
+    Methods
+    ***********************/
+
+    /**********************
+    Attributes
+    ***********************/
+    int top;
+    int stack[STACKSIZE];
+};
+
+#endif //CLASSES_STACK_H
